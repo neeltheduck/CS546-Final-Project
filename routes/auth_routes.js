07705cf8f54621297c1098b088a8d44b5296eb25
tamagi-2 <taramgiblin@gmail.com>
2024-08-21 18:12:34 -0400

    Added error checking

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import {registerUser} from '../data/users.js';
+import {registerUser, loginUser} from '../data/users.js';
+import helper from '../helpers.js';
 // import { Router } from 'express';
 const router = express.Router();
 //need to add session middleware
@@ -21,9 +22,14 @@ router.route('/register').get(async (req, res) => {
     try{
         const userdata = req.body;
         // need to do check for wrong inputs
-        userdata.username = userdata.username.trim();
-        userdata.password = userdata.password.trim();
-        userdata.confirmPassword = userdata.confirmPassword.trim();
+        userdata.username = await helper.checkString(userdata.username, 'Username');
+        userdata.password = await helper.checkString(userdata.password, 'Password');
+        userdata.confirmPassword = await helper.checkString(userdata.confirmPassword, 'Confirm Password');
+        userdata.pronouns = await helper.checkString(userdata.pronouns, 'Pronouns');
+        userdata.bio = await helper.checkString(userdata.bio, 'Bio');
+        userdata.themePreference = await helper.checkString(userdata.themePreference, 'Theme Preference');
+        if (userdata.password !== userdata.confirmPassword) throw 'Error: Password and Confirm Password must be the same';
+        
         console.log("Userdata:", userdata);
         console.log("Username:", userdata.username);
         console.log("Password:", userdata.password);
@@ -32,7 +38,9 @@ router.route('/register').get(async (req, res) => {
         console.log("Bio:", userdata.bio);
         console.log("User Location:", userdata.userLocation);
         console.log("Theme Preference:", userdata.themePreference);
-        const userinfo={username: userdata.username,
+        
+        const userinfo = {
+            username: userdata.username,
             password: userdata.password,
             pronouns: userdata.pronouns,
             bio: userdata.bio,
@@ -40,14 +48,13 @@ router.route('/register').get(async (req, res) => {
             themePreference: userdata.themePreference
         };
 
-        const insertuser= await registerUser(userinfo);
+        const insertuse r= await registerUser(userinfo);
         console.log("Insert User:");
         console.log(insertuser);
-        if(insertuser){
+        if (insertuser) {
             console.log("User Registered");
             return res.redirect('/login');
-        }
-        else{
+        } else {
             return res.status(500).json({error: 'User not registered'});
         }
     } catch (e) {
@@ -58,7 +65,7 @@ router.route('/register').get(async (req, res) => {
 
 router.route('/login').get(async (req, res) => {
     //code here for GET
-    try{
+    try {
         return res.render('login', {themePreference: 'light', title: 'Login'});
     } catch (e) {
         console.log(e);
@@ -68,19 +75,27 @@ router.route('/login').get(async (req, res) => {
     .post(async (req, res) => {
         //code here for POST
     const logindata = req.body;
-    logindata.Username= logindata.Username.trim();
-    logindata.Password= logindata.Password.trim();
-    console.log("Username:", logindata.Username);
-    console.log("Password:", logindata.Password);
-    const user= await loginUser(logindata.Username, logindata.Password);
-    console.log("User:");
-    console.log(user);
-    if(user){
-        console.log("User Logged In");
-        return res.redirect('/user');
+    try {
+        logindata.username = await helper.checkString(loginData.username, 'Username');
+        logindata.password = await helper.checkString(loginData.password, 'Password');
+        console.log("Username:", logindata.Username);
+        console.log("Password:", logindata.Password);
+    } catch (e) {
+        return res.status(500).render('login', {error: e});
     }
-    else{
-        return res.status(500).json({error: 'User not logged in'});
+
+    try {
+        const user = await loginUser(logindata.username, logindata.password);
+        console.log("User:");
+        console.log(user);
+        if (user){
+            console.log("User Logged In");
+            return res.redirect('/user');
+        } else {
+            return res.status(500).render('login', {error: 'User not logged in'});
+        }
+    } catch (e) {
+        return res.status(500).render('login', {error: e});
     }
     });
 
