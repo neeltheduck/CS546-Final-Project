f6d714552294a6c0840a1459f0954e9018b8bf65
tamagi-2 <taramgiblin@gmail.com>
2024-08-21 18:13:28 -0400

    Added more error checking to checkString

--- helpers.js
+++ helpers.js
@@ -18,7 +18,17 @@ const checkString = async (str, varName) => {
     // Idk what other constraints to add
     if ((varName === 'toolName' || varName === 'location') && str.search(/[0-9]/) !== -1) throw `Error: ${varName} cannot contain numbers`;
     if ((varName === 'description' || varName === 'condition') && str.length < 20) throw `Error: ${varName} must be at least 20 characters long`;
+    if (varName === 'Username' && (str.length < 5 || str.length > 10)) throw `Error: ${varName} must be (inclusive) 5-10 characters long`;
 
+    if (varName === 'Password' || varName === 'Confirm Password') {
+        if (str.length < 8) throw `Error: ${varName} must be at least 8 characters long`;
+        if (str.search(/[A-Z]/) === -1 || str.search(/[0-9]/) === -1 || str.search(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/>~]/) === -1) throw `Error: ${varName} must containt at least one uppercase letter, one number, and one special character`;
+    }
+
+    if (varName == 'Pronouns' && (str.toLowerCase() !== 'he/him' && str.toLowerCase() !== 'she/her' && str.toLowerCase() !== 'they/them')) throw `Error: ${varName} must either be He/Him, She/Her, or They/Them`;
+    if (varName === 'Bio' && (str.length < 20 || str.length > 255)) throw `Error: ${varName} must be (inclusive) 25-255 characters long`;
+    if (varName === 'Theme Preference' && (str.toLowerCase() !== 'light' && str.toLowerCase() !== 'dark')) throw `Error: ${varName} must be either 'Light' or 'Dark'`;
+    
     return str;
 };
 
