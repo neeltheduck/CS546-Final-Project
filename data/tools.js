20049923d1b281b5e6c20ff6978f02b5ac4e4f68
tamagi-2 <taramgiblin@gmail.com>
2024-08-21 16:43:38 -0400

    Added error handling to addTool
    May need to update/change in the future

--- data/tools.js
+++ data/tools.js
@@ -5,6 +5,22 @@ import helper from '../helpers.js';
 // addTool
 export const addTool = async ({toolName, description, condition, userID, availability, location, images}) => {
     try {
+        toolName = await helper.checkString(toolName, 'Tool Name');
+        description = await helper.checkString(description, 'Description');
+        condition = await helper.checkString(condition, 'Condition');
+        userID = await helper.checkId(userID, 'User ID');
+        location = await helper.checkString(location, 'Location');
+
+        if (!availability) throw 'Error: Availability with a Start and End Date for the tool must be provided';
+        availability.start = await helper.checkDate(availability.start, 'Availability Start Date');
+        availability.end = await helper.checkDate(availability.end, 'Availability End Date');
+
+        if (!images) throw 'Error: Images must be provided';
+        if (!images.isArray()) throw 'Error: Images must be an array of tool images';
+        for (let image in images) {
+            image = await helper.checkString(image, 'Image');
+        }
+        
         const toolCollection = await tools();
         const dateAdded = new Date().toLocaleDateString();
         const newTool = {toolName, description, condition, userID, dateAdded, availability, location, images};
@@ -15,6 +31,7 @@ export const addTool = async ({toolName, description, condition, userID, availab
             throw `Error: Tool with name ${toolName} already exists, find a new name.`;
         }
         const result = await toolCollection.insertOne(newTool);
+        if (!result.acknowledged || !result.insertedId) throw 'Error: Tool could not be inserted into database';
         console.log("Tool added successfully.");
         console.log(result);
         return result;
@@ -42,7 +59,7 @@ export const getToolWithID = async (id) => {
 };
 
 // updateTool
-export const updateTool = async (toolID, toolName, description, condition, userID, dateAdded, availability, location, images) => {
+export const updateTool = async ({toolID, toolName, description, condition, userID, dateAdded, availability, location, images}) => {
     toolID = await checkId(toolID, 'Tool ID');
     toolName = await helper.checkString(toolName, 'Tool Name');
     description  = await helper.checkString(description, 'Description');
