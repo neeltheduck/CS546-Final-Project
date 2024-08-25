e29403e34ca77e36e40048d504782649ef35f926
neeltheduck <nmeyyur@stevens.edu>
2024-08-24 20:51:32 -0400

    user fix

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -8,14 +8,27 @@ const router = Router();
 router
 .get('/:username', async (req, res) => {
     try {
-        let currentUsername = req.body.user.username;
+        let currentUsername = req.params.username;
         let user = await getUser(currentUsername);
 
         if (!user){
             return res.status(404).send('Sorry! User not found.');
         }
 
-        res.json(user);
+        res.render('users', {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+            pronouns: user.pronouns,
+            bio: user.bio,
+            userLocation: user.userLocation,
+            themePreference: user.themePreference,
+            listedTools: user.listedTools,
+            borrowedTools: user.borrowedTools,
+            reservationHistory: user.reservationHistory,
+            tradeStatuses: user.tradeStatuses,
+            wishList: user.wishList,
+        });
     } catch (error) {
         res.status(500).send('Error: Internal Server Error.');
     }
